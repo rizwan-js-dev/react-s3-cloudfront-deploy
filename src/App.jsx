d11750981c1f188bc04b002a13c444ed9bad5fee
Rizwan Shaikh <rizwanshaikh@Rizwans-MacBook-Pro.local>
2026-07-28 11:33:57 +0530

    Add Env For Test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>
+            {import.meta.env.VITE_API_MESSAGE}
+          </h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
